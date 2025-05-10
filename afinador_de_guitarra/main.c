a48f8ba620f3d07967ffba08a9ada3b84c86de64
GilPM1 <jpm6100019@lasallebajio.edu.mx>
2025-05-10 10:06:38 -0600

    main 1

---
+++ afinador_de_guitarra/main.c
@@ -0,0 +1,188 @@
+#include <msp430.h> 
+
+// Enumeración para representar las notas musicales que se afinan
+typedef enum {
+    E2, // 82 Hz
+    A, // 110 Hz
+    D, // 147 Hz
+    G, // 196 Hz
+    B, // 247 Hz
+    E4  // 333Hz
+} TiposNotas;
+
+// Estructura que asocia una nota con su frecuencia
+typedef struct {
+    TiposNotas nota;
+    unsigned int frequencia;
+} Notas;
+
+// Tabla de notas de referencia
+Notas tablaNotas[] = {
+    {E2, 82},
+    {A, 110},
+    {D, 147},
+    {G, 196},
+    {B, 247},
+    {E4, 333},
+};
+
+// Declaración de funciones
+void confg_pines(void);     // Configura los pines de lectura
+void confg_relog(void);           // Configura el temporizador
+
+void mostrar_nota(TiposNotas nota);
+TiposNotas detectarNota(unsigned int f);
+void indicarAfinacion(unsigned int freqEntrada, Notas notaRef);
+
+
+
+
+
+// Variables globales
+float factor = 0.000004;          // Factor de conversión de cuentas a segundos (asume 250 kHz SMCLK)
+unsigned int frecuencia = 0;      // Resultado de la frecuencia
+unsigned char estado_anterior = 1; // Estado anterior del pin
+unsigned int cuenta = 0;          // Contador de ciclos del timer
+float periodo = 0;                // Periodo medido
+unsigned char nueva_medida = 0;
+
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;     // Stop watchdog timer
+
+    confg_pines();
+    confg_relog();
+
+    P4DIR = 0x01;                 // Pin para salida de observación en osciloscopio
+    P4OUT = 0x00;
+
+    __enable_interrupt();         // Habilitar interrupciones globales
+
+    while(1){
+        if (nueva_medida) {
+            nueva_medida = 0;
+
+
+            // Detectar la nota más cercana
+            TiposNotas nota = detectarNota(frecuencia);
+
+            // Mostrar la nota en display
+            mostrar_nota(nota);
+
+            // Indicar si la afinación está baja, precisa o alta
+            indicarAfinacion(frecuencia, tablaNotas[nota]);
+        }
+    }
+
+    return 0;
+}
+
+void confg_pines(void){
+
+    //pin de lectura
+    P6DIR = 0x00;     // Pin P6.0 como entrada
+    P6REN |= 0x01;    // Habilitar resistencia en P6.0
+    P6OUT |= 0x01;    // Configurar como pull-up
+
+    //led indicador de lectura
+    P1DIR = 0x01;     // LED como salida
+    P1OUT = ~0x01;     // LED apagado inicialmente
+
+    //leds display
+    P2DIR = 0xFD;     // LEDs del display 2.0, 2.2 2.3 2.4 2.5 2.6 2.7   FR = 1111 1101
+    P2OUT = ~0xFD;     // LEDs apagados inicialmente
+
+
+    //leds indicadores
+    P3DIR = 0x1F;     // LEDs indicadores   3.0 3.1 3.2 3.3 3.4
+    P3OUT = ~0x1F;     // LED apagado inicialmente
+
+}
+
+void confg_relog(void){
+    TA0CCTL0 = CCIE;                         // Habilitar interrupción CCR0
+    TA0CCR0 = 1000 - 1;                      // Cuenta hasta 999
+    TA0CTL = TASSEL_2 | MC_1 | ID_0 | TACLR; // SMCLK, modo up, sin división
+}
+
+
+
+void mostrar_nota(TiposNotas nota) {
+    const unsigned char segmentos[] = {
+        0xF1, // E
+        0xED, // A
+        0xF8, // D
+        0x79, // G
+        0xBC, // B
+        0xF1, // E
+
+    };
+    P2OUT = (P2OUT & ~0xFD) | segmentos[nota]; // Aplicar solo a segmentos
+}
+
+
+// Devuelve la nota más cercana a una frecuencia dada
+TiposNotas detectarNota(unsigned int f) {
+    // Calcula la diferencia absoluta entre la frecuencia medida y la frecuencia de referencia de cada nota
+    unsigned int dif0 = (f > tablaNotas[0].frequencia) ? f - tablaNotas[0].frequencia : tablaNotas[0].frequencia - f; // Diferencia con E2 (82 Hz)
+    unsigned int dif1 = (f > tablaNotas[1].frequencia) ? f - tablaNotas[1].frequencia : tablaNotas[1].frequencia - f; // Diferencia con A2 (110 Hz)
+    unsigned int dif2 = (f > tablaNotas[2].frequencia) ? f - tablaNotas[2].frequencia : tablaNotas[2].frequencia - f; // Diferencia con D3 (147 Hz)
+    unsigned int dif3 = (f > tablaNotas[3].frequencia) ? f - tablaNotas[3].frequencia : tablaNotas[3].frequencia - f; // Diferencia con G3 (196 Hz)
+    unsigned int dif4 = (f > tablaNotas[4].frequencia) ? f - tablaNotas[4].frequencia : tablaNotas[4].frequencia - f; // Diferencia con B3 (247 Hz)
+    unsigned int dif5 = (f > tablaNotas[5].frequencia) ? f - tablaNotas[5].frequencia : tablaNotas[5].frequencia - f; // Diferencia con E4 (333 Hz)
+
+    // Inicializa la variable "menor" con la primera diferencia para comparar después
+    unsigned int menor = dif0;
+
+    TiposNotas mejor = E2;
+
+    if (dif1 < menor) { menor = dif1; mejor = A; }
+    if (dif2 < menor) { menor = dif2; mejor = D; }
+    if (dif3 < menor) { menor = dif3; mejor = G; }
+    if (dif4 < menor) { menor = dif4; mejor = B; }
+    if (dif5 < menor) { menor = dif5; mejor = E4; }
+
+    return mejor;
+}
+
+
+
+void indicarAfinacion(unsigned int freqEntrada, Notas notaRef) {
+    P3OUT &= ~0x1F;
+
+    unsigned char porcentaje = freqEntrada * 0.06;
+    unsigned char sostenido = notaRef.frequencia + porcentaje;
+    unsigned char bemol = notaRef.frequencia - porcentaje;
+
+    if (freqEntrada < notaRef.frequencia)
+        P3OUT |= 0x01; //bemol
+    else if (freqEntrada == bemol)
+        P3OUT |= 0x04; // Leve baja
+    else if (freqEntrada == notaRef.frequencia)
+        P3OUT |= 0x10; // Afinado
+    else if (freqEntrada == sostenido)
+        P3OUT |= 0x08; // sostenido
+    else
+        P3OUT |= 0x02; // Muy alta
+}
+
+
+
+
+
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void TIMER0_A0_ISR(void){
+    unsigned char estado_actual = (P6IN & 0x01);
+
+    // Detectar flanco de subida
+    if ((estado_actual == 1) && (estado_anterior == 0)) {
+        periodo = cuenta * factor;
+        if (periodo > 0.00000)
+        frecuencia = 1 / periodo;
+        cuenta = 0;
+    }
+
+    estado_anterior = estado_actual;
+    cuenta++;
+    nueva_medida = 1;
+}
